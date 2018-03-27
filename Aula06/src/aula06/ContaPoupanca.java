b6aab104ca8063da2d64aa24e177d09ce62ae58c
Anglya <36281626+Anglya@users.noreply.github.com>
2018-03-26 22:34:10 -0300

    Add files via upload

---
+++ Aula06/src/aula06/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package aula06;
+
+public class ContaPoupanca extends ContaBancaria{
+    private int limite;
+    
+    public double Sacar(){
+        return -1;
+    }
+    public double Depositar(){
+        return -1;
+    }
+}
